98468a281b45c45252e2adc012e2f3cc99978761
Coetzee van Staden <coetzee.v@superbalist.com>
2018-08-14 16:31:04 +0200

    modified event handler to pick up different models for insurance quote

--- Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -6,7 +6,11 @@ All Rights Reserved.
 Confidential and Proprietary - Protected under copyright and other laws.
 ==============================================================================*/
 
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using Vuforia;
 
 /// <summary>
@@ -54,13 +58,13 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
             newStatus == TrackableBehaviour.Status.TRACKED ||
             newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
         {
-            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            Debug.Log("Trackable !!" + mTrackableBehaviour.TrackableName + " found");
             OnTrackingFound();
         }
         else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
                  newStatus == TrackableBehaviour.Status.NO_POSE)
         {
-            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            Debug.Log("Trackable !!" + mTrackableBehaviour.TrackableName + " lost");
             OnTrackingLost();
         }
         else
@@ -93,6 +97,8 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
         // Enable canvas':
         foreach (var component in canvasComponents)
             component.enabled = true;
+
+        CallApi();
     }
 
 
@@ -116,4 +122,98 @@ public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandle
     }
 
     #endregion // PROTECTED_METHODS
+
+    /* Create a Root Object to store the returned json data in */
+    [System.Serializable]
+    public class Quotes
+    {
+        public Quote[] values;
+    }
+
+    [System.Serializable]
+    public class Quote
+    {
+        public string package_name;
+        public string sum_assured;
+        public int base_premium;
+        public string suggested_premium;
+        public string created_at;
+        public string quote_package_id;
+        public QuoteModule module;
+    }
+
+    [System.Serializable]
+    public class QuoteModule
+    {
+        public string type;
+        public string make;
+        public string model;
+    }
+
+    [Serializable]
+    public struct Param
+    {
+        public string key;
+        public string value;
+    }
+
+    public string api_key = "sandbox_NzRkZGUxNTQtNWUxZC00NzI1LWI2MDAtOTM1ZWY5NjAzZmM1LmlacEpPLUdTbXBMZV9kT2RCRmlTSUExVktRSC1mbzlf";
+
+    public void CallApi()
+    {
+        Debug.Log("Started");
+        string obje = getBrand(mTrackableBehaviour.TrackableName);
+        StartCoroutine(CreateQuoteCoroutine(obje, mTrackableBehaviour.TrackableName));
+    }
+
+    public string getBrand(String trackableName)
+    {
+        Debug.Log("Started getBrand.");
+        string bra = "";
+        if (trackableName == "Skull")
+        {
+            bra = "iPhone 6S 64GB LTE";
+        }
+        else if (trackableName == "Clover")
+        {
+            bra = "Galaxy S8 64GB LTE";
+        }
+
+        Debug.Log(bra + " ended at getBrand.");
+
+        return bra;
+    }
+
+    IEnumerator CreateQuoteCoroutine(String modelNr, String trackableName)
+    {
+
+        string auth = api_key + ":";
+        auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+        auth = "Basic " + auth;
+
+        WWWForm form = new WWWForm();
+        form.AddField("type", "root_gadgets");
+        form.AddField("model_name", modelNr);
+
+        UnityWebRequest www = UnityWebRequest.Post("https://sandbox.root.co.za/v1/insurance/quotes", form);
+        www.SetRequestHeader("AUTHORIZATION", auth);
+        yield return www.Send();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+           Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
+
+            String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
+            Debug.Log(modelNr + ": " + json.values[0].module.make);
+            Debug.Log(text);
+            Debug.Log("Form upload complete!");
+            TextMesh textObject = GameObject.Find(trackableName + "/Text").GetComponent<TextMesh>();
+            textObject.text = text;
+        }
+        yield return true;
+    }
 }
